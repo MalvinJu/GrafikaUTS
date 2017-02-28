85cd798feaf52861264df03036287a97752bb97d
albertlo96 <albert.lo96@gmail.com>
2017-02-28 18:03:22 +0700

    Add file parser 'readfile.cpp'

--- readfile.cpp
+++ readfile.cpp
@@ -1,25 +1,88 @@
-#include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "Point.h"
+
 using namespace std;
 
-void readBangunan(){
+vector<vector<Point>> readBangunanAndJalan(string path){
 	ifstream infile;
 	string sline;
-	infile.open("bgn.txt"); // open file bangunan
-	
+	infile.open(path); // open file bangunan
+	vector<vector<Point>> VP; //Vector of vector of points
+	vector<Point> vec_temp;
 	if (!infile.good()){
 		cout << "File not found";
 	} else {
 		while(!infile.eof()){
 	        getline(infile, sline); // save satu baris ke sline
-	        cout << sline; // Prints our STRING.
+	        if(sline.length()<=2){
+				//no action
+				VP.push_back(vec_temp);
+				vec_temp.clear();
+			} else {
+				stringstream  lineStream(sline);
+				int x;
+				int y;
+				// Read an integer at a time from the line
+				lineStream >> x;
+				lineStream >> y;
+				Point temp(x,y);
+				vec_temp.push_back(temp);
+				//cout<<"X:"<<x<<" | ";
+				//cout<<"Y:"<<y<<endl;
+			}
 		}
 	}
+	VP.push_back(vec_temp);
 	
 	infile.close(); // close file
+	return VP;
+}
+
+vector<Point> readPohon(string path){
+	ifstream infile;
+	string sline;
+	infile.open(path); // open file bangunan
+	vector<Point> VP; //Vector of vector of points
+	if (!infile.good()){
+		cout << "File not found";
+	} else {
+		while(!infile.eof()){
+	        getline(infile, sline); // save satu baris ke sline
+			stringstream  lineStream(sline);
+			int x;
+			int y;
+			// Read an integer at a time from the line
+			lineStream >> x;
+			lineStream >> y;
+			Point temp(x,y);
+			VP.push_back(temp);
+			//cout<<"X:"<<x<<" | ";
+			//cout<<"Y:"<<y<<endl;			
+		}
+	}
+	infile.close(); // close file
+	return VP;
+	
 }
 
 int main() {
-	readBangunan();
+	vector<vector<Point>> VP;
+	VP = readBangunanAndJalan("DataGambar/dataBangunan.txt"); 
+	for(int i=0; i< VP.size();i++){
+		cout<<"--------"<<endl;
+		for(int j=0; j<VP[i].size(); j++){
+			cout<<" X "<<VP[i][j].getX()<<" ";
+			cout<<" Y "<<VP[i][j].getY()<<endl;
+		}
+	}
+	vector<Point> Pohon;
+	Pohon = readPohon("DataGambar/dataPohon.txt");
+	for(int i=0; i< Pohon.size();i++){
+		cout<<" X "<<Pohon[i].getX()<<" ";
+		cout<<" Y "<<Pohon[i].getY()<<endl;
+	}
 }
